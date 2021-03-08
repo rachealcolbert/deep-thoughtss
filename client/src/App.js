44f43d09cbece37425888d70f89df5bca0bb14ea
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-07 20:34:02 -0700

    issues with app

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import NoMatch from "./pages/NoMatch";
-import SingleThought from "./pages/SingleThought";
-import Profile from "./pages/Profile";
-import Signup from "./pages/Signup";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -21,19 +15,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/thought" component={SingleThought} />
-          </div>
-          <Footer />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
         </div>
-      </Router>
+        <Footer />
+      </div>
     </ApolloProvider>
   );
 }
